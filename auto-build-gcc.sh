e60feb468e574b051718f8c5b45b44b1db2ee7ad
JeComtempleDuCodeSource <69819391+JeComtempleDuCodeSource@users.noreply.github.com>
2020-11-14 23:08:09 +0100

    Add files via upload

---
+++ auto-build-gcc.sh
@@ -0,0 +1,6 @@
+cd "./Source"
+cmake -B"./build" -S"./"
+cd "./build"
+make
+echo "Press any key to exit..."
+read quit
---
+++ auto-build-mingw.cmd
@@ -0,0 +1,5 @@
+cd "./Source"
+cmake -G"MinGW Makefiles" -B"./build" -S"./"
+cd "./build"
+mingw32-make
+PAUSE
